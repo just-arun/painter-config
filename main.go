1ced672cb47b21d8d1561813299c81d3b5364c4a
arun <arunberry47@gmail.com>
2021-05-19 18:43:20 +0530

    adding main function

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "github.com/just-arun/painter-config/config"
+import (
+	"fmt"
+
+	"github.com/just-arun/painter-config/config"
+)
 
 var (
 	Init func(path string, name string, fileType string)                                          = config.InitConfig
 	Load func(path string, name string, fileType string) (configuration config.Config, err error) = config.LoadVars
 )
+
+func main() {
+	fmt.Println("painter config package...")
+}
